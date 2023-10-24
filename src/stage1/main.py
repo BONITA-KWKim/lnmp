1d076009268a87ee11a8f88c20e56c1c515e3597
TITEDIOS Kyungwon.KIM <kyungwon9010@hanmail.net>
2023-10-24 17:18:40 +0900

    developing pytorch ligntningAI

--- src/stage1/main.py
+++ src/stage1/main.py
@@ -9,11 +9,11 @@ import torchvision.transforms as T
 
 # from models.model import TDClassifier
 from models.model import LitTDClassifier
-from models.model import LitTDDataModule
+from dataset import LitTDDataModule
 from models.model import TDLightningCLI
 # from dataset import PatchDataset
 
-import pickle
+# import pickle
 import mlflow.pytorch
 # from mlflow.models import infer_signature
 
@@ -25,15 +25,20 @@ from lightning.pytorch.callbacks import (
 )
 
 
+# import datetime
+from datetime import datetime
 def cli_main():
   early_stopping = EarlyStopping(
-    monitor="val_loss",
+    monitor="val_loss", patience=10
   )
 
-  if os.path.join(os.getcwd(), "results") is False:
-    os.makedirs(os.path.join(os.getcwd(), "results"))
+  date_ = datetime.now().strftime('%Y%m%d-%H%M')
+  model_saved_dir = os.path.join(os.getcwd(), "results", date_)
+  if os.path.join(model_saved_dir) is False:
+    os.makedirs(model_saved_dir)
   checkpoint_callback = ModelCheckpoint(
-    dirpath=os.path.join(os.getcwd(), "results"), save_top_k=1, verbose=True, monitor="val_loss", mode="min"
+    dirpath=os.path.join(model_saved_dir), filename="best", save_top_k=1, 
+    verbose=False, monitor="val_loss", mode="min"
   )
 
   lr_logger = LearningRateMonitor()
@@ -42,15 +47,27 @@ def cli_main():
     LitTDDataModule,
     run=False,
     save_config_callback=None,
-    trainer_defaults={"callbacks": [early_stopping, checkpoint_callback, lr_logger]},
+    trainer_defaults={
+      "callbacks": [early_stopping, checkpoint_callback, lr_logger]
+    },
   )
+  # set run name
+  mlflow.set_tag("mlflow.runName", "Thyroid-V0.1-Detection-"+date_) 
 
   if cli.trainer.global_rank == 0:
       mlflow.pytorch.autolog()
   # cli.model=torch.compile(cli.model)
   cli.trainer.fit(cli.model, datamodule=cli.datamodule)
-  cli.trainer.test(ckpt_path="best", datamodule=cli.datamodule)
+  # cli.trainer.test(ckpt_path="best", datamodule=cli.datamodule)
+  cli.trainer.test(ckpt_path=os.path.join(model_saved_dir, 'best.ckpt'), datamodule=cli.datamodule)
 
+  """Predict
+  model = LightningTransformer.load_from_checkpoint(PATH)
+  dataset = WikiText2()
+  test_dataloader = DataLoader(dataset)
+  trainer = pl.Trainer()
+  predictions = trainer.predict(model, dataloaders=test_dataloader)
+  """
 
 if __name__=="__main__":
   # main()
@@ -60,10 +77,10 @@ if __name__=="__main__":
 # find . -type d | xargs -n1 -I{} rmdir {}
 '''20231019-Thyroid
 [v0.1]
-mlflow run . -P mode=train -P cancer_type=thyroid -P backbone=resnext50_32x4d \
--P epoch=50 -P bsize=32 -P num_class=3 \
--P train_dir=/data/rnd1712/dataset/thyroid/classification/v0-1 \
--P infer_dir=/data/rnd1712/dataset/thyroid/classification/v0-1 
+mlflow run . -P backbone=resnext50_32x4d \
+-P max_epochs=100 -P batch_size=32 -P num_class=3 \
+-P dataset_dir=/data/rnd1712/dataset/thyroid/classification/v0-1 &> logs/20231024N001-V0_1.log &
+tail -f logs/20231024N001-V0_1.log
 
 mlflow run . -P mode=test -P cancer_type=thyroid -P backbone=resnext50_32x4d \
 -P epoch=50 -P bsize=32 -P num_class=3 \
@@ -72,8 +89,8 @@ mlflow run . -P mode=test -P cancer_type=thyroid -P backbone=resnext50_32x4d \
 
 [v0.2]
 mlflow run . -P backbone=resnext50_32x4d \
--P max_epochs=100 -P batch_size=128 -P num_class=2 \
--P dataset_dir=/data/rnd1712/dataset/thyroid/classification/v0-2 &> /dev/null &
+-P max_epochs=2 -P batch_size=64 -P num_class=2 \
+-P dataset_dir=/data/rnd1712/dataset/thyroid/classification/v0-2 &> logs/20231024N005-V0_2.log &
 
 mlflow run . -P mode=test -P cancer_type=thyroid -P backbone=resnext50_32x4d \
 -P bsize=64 -P num_class=2 \
--- src/stage1/models/model.py
+++ src/stage1/models/model.py
@@ -1,13 +1,13 @@
 import os
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, ConcatDataset
+# from torch.utils.data import DataLoader, ConcatDataset
 
 # import lightning.pytorch as pl
 import lightning as L
 from lightning.pytorch.cli import LightningCLI
 
-from dataset import PatchDataset
+# from dataset import PatchDataset
 
 
 def get_model_by_name(modelname, pretrained):
@@ -53,92 +53,6 @@ class TDClassifier(nn.Module):
     self.cnn.load_state_dict(pretrained_dict)
 
 
-class LitTDDataModule(L.LightningDataModule):
-  def __init__(self, dataset_dir, num_class, batch_size, num_workers):
-    """
-    Initialization of inherited lightning data module
-    """
-    super(LitTDDataModule, self).__init__()
-    self.dataset_dir = dataset_dir
-    self.train_dataset = None
-    self.val_dataset = None
-    self.test_dataset = None
-    self.batch_size = batch_size
-    self.num_workers = num_workers
-    self.num_class = num_class
-    # self.num_class = len(os.listdir(dataset_dir))
-    self.train_count = None
-    self.val_count = None
-    self.test_count = None
-    self.RANDOM_SEED = 42
-
-  def setup(self, stage=None):
-    """
-    Split the data into train, test, validation data
-
-    :param stage: Stage - training or testing
-    """
-    if stage=="fit":
-      for dataset_type in ["train", "val", "test"]:
-        _list = []
-        for label in range(self.num_class):
-          path = os.path.join(self.dataset_dir, str(label), dataset_type)
-          _dataset = PatchDataset(path, label, num_classes=self.num_class, 
-                                  one_hot=True)
-          _list.append(_dataset)
-
-        if "train" == dataset_type:
-          self.train_dataset = ConcatDataset(_list)
-        elif "val" == dataset_type:
-          self.val_dataset = ConcatDataset(_list)
-        elif "test" == dataset_type:
-          self.test_dataset = ConcatDataset(_list)
-
-      self.train_count = len(self.train_dataset)
-      self.val_count = len(self.val_dataset)
-      self.test_count = len(self.test_dataset)
-
-    print(f"Number of samples used for training: {self.train_count}")
-    print(f"Number of samples used for validation: {self.val_count}")
-    print(f"Number of samples used for test: {self.test_count}")
-  
-  def create_data_loader(self, source):
-    """
-    Generic data loader function
-
-    :param source: the type of dataset
-
-    :return: Returns the constructed dataloader
-    """
-    datasets = {
-      "train": self.train_dataset,
-      "val": self.val_dataset,
-      "test": self.test_dataset,
-    }
-
-    return DataLoader(datasets[source], batch_size=self.batch_size, 
-                      shuffle=False, drop_last=False, 
-                      num_workers=self.num_workers)
-  
-  def train_dataloader(self):
-    """
-    :return: output - Train data loader for the given input
-    """
-    return self.create_data_loader(source="train")
-
-  def val_dataloader(self):
-    """
-    :return: output - Validation data loader for the given input
-    """
-    return self.create_data_loader(source="val")
-
-  def test_dataloader(self):
-    """
-    :return: output - Test data loader for the given input
-    """
-    return self.create_data_loader(source="test")
-    
-
 class LitTDClassifier(L.LightningModule):
   
   def __init__(self, num_class, backbone='resnext50_32x4d', pretrained=True, 
@@ -178,29 +92,27 @@ class LitTDClassifier(L.LightningModule):
     
   def forward(self, x):
     """
-    :param x: input data
+    :param x -  input data
 
     :return: output - Type of news for the given news snippet
     """
     output = self.cnn(x)
     output = self.classifier(output)
-    output = nn.functional.softmax(output, 1)
+    output = nn.functional.softmax(output, dim=1)
     return output
   
   def training_step(self, batch, batch_idx):
     """
     Training the data as batches and returns training loss on each batch
 
-    :param train_batch Batch data
-    :param batch_idx: Batch indices
+    :param batch - Batch data
+    :param batch_idx - Batch indices
 
     :return: output - Training loss
     """
     inputs, target = batch
 
-    output = self.cnn(inputs)
-    output = self.classifier(output)
-    output = nn.functional.softmax(output, 1)
+    output = self(inputs)
     loss = torch.nn.functional.mse_loss(output, target)
     self.log(
       "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, 
@@ -209,15 +121,13 @@ class LitTDClassifier(L.LightningModule):
     return {"loss": loss}  
 
   def configure_optimizers(self):
-    self.optimizer = torch.optim.Adam(
-      self.parameters(), lr=self.lr, weight_decay=self.weight_decay
-    )
+    self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
     self.scheduler = {
       "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(
           self.optimizer,
           mode="min",
-          factor=0.2,
-          patience=2,
+          factor=self.weight_decay,
+          patience=10,
           min_lr=1e-6,
           verbose=True,
       ),
@@ -228,12 +138,14 @@ class LitTDClassifier(L.LightningModule):
 
   def validation_step(self, batch, batch_idx):
     inputs, target = batch
-    output = self.cnn(inputs)
-    output = self.classifier(output)
-    output = nn.functional.softmax(output, 1)
+
+    output = self(inputs)
     loss = torch.nn.functional.mse_loss(output, target)
-    self.val_outputs.append(loss)      
-    correct = (torch.argmax(output, -1) == torch.argmax(target, -1)).sum().item()
+    self.val_outputs.append(loss)
+
+    preds = torch.argmax(output, -1)
+    gt = torch.argmax(target, -1)
+    correct = (preds == gt).sum().item()
 
     self.log(
       "val_loss", loss, logger=True, sync_dist=True
@@ -253,12 +165,13 @@ class LitTDClassifier(L.LightningModule):
 
   def test_step(self, batch, batch_idx):
     inputs, target = batch
-    output = self.cnn(inputs)
-    output = self.classifier(output)
-    output = nn.functional.softmax(output, 1)
+
+    output = self(inputs)
     loss = torch.nn.functional.mse_loss(output, target)
-    self.test_outputs.append(loss)   
-    correct = (torch.argmax(output, -1) == torch.argmax(target, -1)).sum().item()
+
+    preds = torch.argmax(output, -1)
+    gt = torch.argmax(target, -1)
+    correct = (preds == gt).sum().item()
 
     self.log(
       "test_loss", loss, logger=True, sync_dist=True
@@ -267,17 +180,25 @@ class LitTDClassifier(L.LightningModule):
       "test_acc", correct/target.shape[0], logger=True, sync_dist=True
     )
     test_acc = correct/target.shape[0]
+    self.test_outputs.append((preds == gt).sum()/target.shape[0])   
+
     return {"test_acc": test_acc}
 
   def on_test_epoch_end(self):
     """
     Computes average test accuracy score
     """
-    print(self.test_outputs)
     avg_test_acc = torch.stack(self.test_outputs).mean()
     self.log("avg_test_acc", avg_test_acc)
     self.test_outputs.clear()
 
+  def predict_step(self, batch, batch_idx, dataloader_idx=0):
+    inputs, _ = batch
+
+    output = self(inputs)
+    preds = torch.argmax(output, -1)
+
+    return preds
 
 class TDLightningCLI(LightningCLI):
   def add_arguments_to_parser(self, parser):
